acbb83ca3cb89c76ca8fb6940cd564273298a544
andreaspencer <andreaaddawn@gmail.com>
2021-10-09 15:16:43 -0700

    trasnfered markdown

--- Develop/README.md
+++ Develop/README.md
@@ -1,2 +1,61 @@
-# home
+# jfdas
 
+  ## Licensing:
+    function renderLicenseBadge(license) {
+  const licenseType = license.license;
+  const yourLicense = ''
+  if (licenseType === 'MIT') {
+    yourLicense = `![License](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  } else if (licenseType === 'Apache 2.0') {
+    yourLicense = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]`
+  } else if (licenseType === 'Boost 1.0') {
+    yourLicense = `[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)]`
+  } else {
+    license.license = "N/A"
+  }
+  return yourLicense;
+}
+    function renderLicenseLink(license) {
+  const licenseType = license.license;
+  const yourLink = ''
+  if (licenseType === 'MIT') {
+    yourLink = `(https://opensource.org/licenses/MIT)`
+  } else if (licenseType === 'Apache 2.0') {
+    yourLink = `(https://opensource.org/licenses/Apache-2.0)`
+  } else if (licenseType === 'Boost 1.0') {
+    yourLink= `(https://www.boost.org/LICENSE_1_0.txt)`
+  } else {
+    license.license = "N/A"
+  }
+  return yourLink;
+}
+    
+    ## Table of Contents
+    -[Description](#description)
+    -[Installation](#installation)
+    -[Usage](#usage)
+    -[Contribution](#contribution)
+    -[Testing](#testing)
+    -[Questions](#questions)
+
+    ### Description:
+    fadssdfdsafdsfdfdfdsfdsa
+
+    ### Installation:
+    afdsafdsafdsfdafdsad
+
+    ### Usage:
+    afdsafdsafdsafsfsafdsa
+
+    ### Contribution
+    undefined
+
+    ### Testing 
+    sdfadsfdsafdsaf
+
+    ### License
+    undefined
+
+    ### Questions
+    -Github: [undefined](https://github.com/undefined)
+    -Email: dafsdsafdsafdsfds
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,36 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const licenseType = license.license;
+  const yourLicense = ''
+  if (licenseType === 'MIT') {
+    yourLicense = `![License](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  } else if (licenseType === 'Apache 2.0') {
+    yourLicense = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]`
+  } else if (licenseType === 'Boost 1.0') {
+    yourLicense = `[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)]`
+  } else {
+    license.license = "N/A"
+  }
+  return yourLicense;
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const licenseType = license.license;
+  const yourLink = ''
+  if (licenseType === 'MIT') {
+    yourLink = `(https://opensource.org/licenses/MIT)`
+  } else if (licenseType === 'Apache 2.0') {
+    yourLink = `(https://opensource.org/licenses/Apache-2.0)`
+  } else if (licenseType === 'Boost 1.0') {
+    yourLink= `(https://www.boost.org/LICENSE_1_0.txt)`
+  } else {
+    license.license = "N/A"
+  }
+  return yourLink;
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -14,6 +40,39 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Licensing:
+    ${renderLicenseBadge}
+    ${renderLicenseLink}
+    
+    ## Table of Contents
+    -[Description](#description)
+    -[Installation](#installation)
+    -[Usage](#usage)
+    -[Contribution](#contribution)
+    -[Testing](#testing)
+    -[Questions](#questions)
+
+    ### Description:
+    ${data.description}
+
+    ### Installation:
+    ${data.installation}
+
+    ### Usage:
+    ${data.usage}
+
+    ### Contribution
+    ${data.contribtuion}
+
+    ### Testing 
+    ${data.testing}
+
+    ### License
+    ${data.license}
+
+    ### Questions
+    -Github: [${data.github}](https://github.com/${data.github})
+    -Email: ${data.email}
 `;
 }
 
